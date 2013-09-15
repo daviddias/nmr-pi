c3fd7ac2d8a1da090e1cc6034bf7987ebeb3ce41
David Dias <daviddias.p@gmail.com>
2013-09-15 20:17:26 +0100

    refactor all the way

---
+++ nmrpi.js
@@ -0,0 +1,50 @@
+var io = require('socket.io')
+  , ss = require('socket.io-stream')
+  , path = require('path')
+  , fs = require('fs')
+  , brain = require('./brain.js');
+
+
+
+var start = function (config) {
+  io.listen(config.port);
+
+
+  io.sockets.on('connection', function (socket) {
+
+    ss(socket).on('addController', function(stream, data) {
+      var filename = path.basename(data.name);
+      
+      stream.on('end', function (){
+        brain.addController(config.folderPath + '/' + filename);
+      });
+
+      stream.pipe(fs.createWriteStream(config.folderPath + '/' + filename), function (err, result) {
+        if (err) {console.log(err);}
+      });
+
+    });
+
+
+    socket.on('execute', function(options) {
+      brain.execute(options.controllerID, options.command);
+    });
+
+
+    socket.on('disconnect', function(){
+      console.log('Socket disconnect');
+    });
+  });
+
+};
+
+
+module.exports = {
+  start: start
+};
+
+
+
+
+
+
