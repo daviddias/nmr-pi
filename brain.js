421f838c5a121789e08f335cbd23264874222d61
David Dias <daviddias.p@gmail.com>
2013-09-15 20:59:33 +0100

    refactor all the way

--- brain.js
+++ brain.js
@@ -1,35 +1,35 @@
-var five = require('johnny-five')
-  , path = require('path')
-  , fs = require('fs')
-  , inspect = require('util').inspect
+var five = require('johnny-five');
 
 
-var board = new five.Board()
-board.on("ready", function () {
-  console.log("Board is ready")
-})
+var board = new five.Board({ port: '/dev/ttyAMA0'});
+board.on('ready', function () {
+  console.log('Board is ready');
+});
 
-var controllers = {}
+var controllers = {};
+
 
 var addController = function (path) {
   // 1. Require the controller
-  var controller = require(path)
+  var controller = require(path);
 
   // 2. Add the controller to controller list
-  controllers[controller.id] = controller
-}
+  controllers[controller.id] = controller;
+};
 
 var execute = function(controllerID, functionName) {
-  controllers[controllerID][functionName]()
-}
+  controllers[controllerID][functionName]();
+};
 
-var listControllers = function () {
 
-}
+
+// var listControllers = function () {
+
+// };
 
 
 module.exports = {
   addController: addController,
   execute: execute,
   board: board
-}
+};
